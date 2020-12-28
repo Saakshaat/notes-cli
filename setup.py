02035e2799009b8b98ffe959679ac119fe847fe3
Saakshaat <saakshaat2001@gmail.com>
2020-12-28 15:14:07 +0530

    Update setup.py to prepare for PyPI deployment

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
+import pathlib
+
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
-    name='notes',
+    name='notes-cli',
     version='1.0.0',
+    description="Take notes directly in your terminal.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Saakshaat/notes-cli/",
+    author="Saakshaat Singh",
+    author_email="saakshaat2001@gmail.com",
+    license="MIT",
     py_modules=['notes'],
+    include_package_data=True,
     install_requires=[
-        'Click',
+        'Click'
     ],
     entry_points='''
         [console_scripts]
